358d3423c53be7224eaeaaafb9027e14e329d1b5
yada <trojblue@gmail.com>
2023-07-01 02:14:26 -0400

    init project

---
+++ maroon/cli.py
@@ -0,0 +1,12 @@
+import click
+from .do_format import format_music_folder
+
+@click.command()
+@click.argument('dir_name')
+def cli(dir_name):
+    # Tidy implementation
+    print(f'Tidying directory: {dir_name}')
+    format_music_folder(dir_name)
+
+if __name__ == '__main__':
+    cli()
---
+++ maroon/do_format.py
@@ -0,0 +1,6 @@
+
+def format_music_folder(dir_name):
+    print(f'Tidying directory: {dir_name}')
+
+if __name__ == '__main__':
+    format_music_folder('test')
